ab094e0643d7616d3c9db1857bcff97bf34a96ee
shinjiaki <julio_shinji@hotmail.com>
2025-04-10 17:29:49 -0300

    Lesson 1.3 - Environment variables with zod

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,12 @@
+import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
 
 const server = fastify()
 
 server.register(fastifyCors, { origin: '*' })
+
+console.log(env.DATABASE_URL)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running')
