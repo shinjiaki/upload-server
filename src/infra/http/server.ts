354d8ce657eaea0637fee5c3785d1c598240758e
shinjiaki <julio_shinji@hotmail.com>
2025-04-10 17:58:52 -0300

    Lesson 1.4 - HTTP server with Fastify

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,12 +1,35 @@
-import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.validation,
+    })
+  }
+
+  // Envia o erro p/ alguma ferramenta de observabilidade (Sentry/Datadog/Grafana/OTel)
+
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
+
 server.register(fastifyCors, { origin: '*' })
 
-console.log(env.DATABASE_URL)
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running')
