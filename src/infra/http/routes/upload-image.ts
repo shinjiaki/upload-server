f3fa9ad64919375c5a1796055b6d61fa4f9ee0ca
shinjiaki <julio_shinji@hotmail.com>
2025-04-17 15:20:47 -0300

    Lesson 1.11 - Either: functional error handling

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,6 +1,5 @@
 import { uploadImage } from '@/app/functions/upload-image'
-import { db } from '@/infra/db'
-import { schema } from '@/infra/db/schemas'
+import { isRight, unwrapEither } from '@/shared/either'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
@@ -12,7 +11,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         summary: 'Upload an image',
         consumes: ['multipart/form-data'],
         response: {
-          201: z.object({ uploadId: z.string() }),
+          201: z.null().describe('Image uploaded'),
           400: z.object({ message: z.string() }),
         },
       },
@@ -30,13 +29,22 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         })
       }
 
-      await uploadImage({
+      const result = await uploadImage({
         fileName: uploadedFile?.filename,
         contentType: uploadedFile?.mimetype,
         contentStream: uploadedFile?.file,
       })
 
-      return reply.status(201).send({ uploadId: 'teste' })
+      if (isRight(result)) {
+        return reply.status(201).send()
+      }
+
+      const error = unwrapEither(result)
+
+      switch (error.constructor.name) {
+        case 'InvalidFileFormat':
+          return reply.status(400).send({ message: error.message })
+      }
     }
   )
 }
