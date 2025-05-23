9923e87f3910df9ead47f2cbe2bf58910c9f4f5c
shinjiaki <julio_shinji@hotmail.com>
2025-05-23 15:40:44 -0300

    Lesson 1.13 - Upload files to R2

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -36,6 +36,8 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       })
 
       if (isRight(result)) {
+        console.log(unwrapEither(result))
+
         return reply.status(201).send()
       }
 
