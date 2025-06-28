b43a76ddb45639e7b72f4293b085eeac4229956d
shinjiaki <julio_shinji@hotmail.com>
2025-06-28 19:58:07 -0300

    Lesson 1.20 - Function: export upload (CSV)

--- src/app/functions/export-uploads.ts
+++ src/app/functions/export-uploads.ts
@@ -1,7 +1,11 @@
+import { PassThrough, Transform } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
 import { db, pg } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import { uploadFileToStorage } from '@/infra/storage/upload-file-to-storage'
 import { type Either, makeRight } from '@/shared/either'
-import { asc, count, desc, ilike } from 'drizzle-orm'
+import { stringify } from 'csv-stringify'
+import { ilike } from 'drizzle-orm'
 import { z } from 'zod'
 
 const exportUploadsInput = z.object({
@@ -34,9 +38,48 @@ export async function exportUploads(
 
   const cursor = pg.unsafe(sql, params as string[]).cursor(1)
 
-  for await (const rows of cursor) {
-    console.log(rows)
-  }
+  // for await (const rows of cursor) {
+  //   console.log(rows)
+  // }
 
-  return makeRight({ reportUrl: '' })
+  const csv = stringify({
+    delimiter: ',',
+    header: true,
+    columns: [
+      { key: 'id', header: 'ID' },
+      { key: 'name', header: 'Name' },
+      { key: 'remote_url', header: 'URL' },
+      { key: 'created_at', header: 'Uploaded at' },
+    ],
+  })
+
+  const uploadToStorageStream = new PassThrough()
+
+  const convertToCSVPipeline = pipeline(
+    cursor,
+    new Transform({
+      objectMode: true,
+      transform(chunks: unknown[], encoding, callback) {
+        for (const chunk of chunks) {
+          this.push(chunk)
+        }
+        callback()
+      },
+    }),
+    csv,
+    uploadToStorageStream
+  )
+
+  const uploadToStorage = uploadFileToStorage({
+    contentType: 'text/csv',
+    folder: 'downloads',
+    fileName: `${new Date().toISOString()}-uploads.csv`,
+    contentStream: uploadToStorageStream,
+  })
+
+  const [{ url }] = await Promise.all([uploadToStorage, convertToCSVPipeline])
+
+  console.log(url)
+
+  return makeRight({ reportUrl: url })
 }
